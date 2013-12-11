9d2e7cda2023ad95b2e5cdbe351184971c0aeca2
JJ Merelo <jjmerelo@gmail.com>
2013-12-11 18:17:19 +0100

    Actualizando node

--- node/jseo.js
+++ node/jseo.js
@@ -1,11 +1,20 @@
 #!/usr/bin/node
 
+var fs=require('fs');
 var express = require('express');
 var app = express();
 var problems = new Object;
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.sendfile('256_royal_road.html');
+});
+
+app.get('/js/:file', function(req, res){
+	    var file_name = req.params.file.split(/\./);
+	    console.log(file_name);
+	    var path = file_name[file_name.length-2];
+	    console.log(path);
+	    res.sendfile(path+'.js');
 });
 
 app.put('/problem/:id', function(req,res) {
@@ -24,4 +33,23 @@ app.get('/chromosomes_problem/:id', function(req,res) {
 	    res.send( problems[req.params.id] );
 });
 
+app.get('/best_of_problem/:id', function(req,res) {
+
+	    res.send( best_of( problems[req.params.id] ) );
+});
+
 app.listen(3000);
+
+
+function best_of( un_array ) {
+    var best_so_far= { chromstring: null,
+		       fitness: -1 };
+
+    for ( var i in un_array ) {
+	if ( un_array[i].fitness > best_so_far.fitness ) {
+	    best_so_far = un_array[i];
+	}
+    }
+    return best_so_far;
+    
+}
--- node/test.sh
+++ node/test.sh
@@ -1,4 +1,6 @@
 curl -X PUT http://localhost:3000/problem/rr/
 curl -X PUT http://localhost:3000/problem/rr/chromosome/10111010/fitness/34
-curl -X PUT http://localhost:3000/problem/rr/chromosome/10101010/fitness/33
+curl -X PUT http://localhost:3000/problem/rr/chromosome/10101012/fitness/33
+curl -X PUT http://localhost:3000/problem/rr/chromosome/10101013/fitness/88
 curl http://localhost:3000/chromosomes_problem/rr
+curl http://localhost:3000/best_of_problem/rr
